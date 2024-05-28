e888289790eaf66daaee89025b9beb1cbb6ae8f5
Arthur Melo <119857587+ArthMelo@users.noreply.github.com>
2024-05-27 22:29:03 -0300

    Update concatenar.c

--- concatenar.c
+++ concatenar.c
@@ -1,33 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
-char *concatena(char *s1, char *s2);
+char *concatena(char *s1, char *s2)
+{
+    int tam1, tam2;
+    char *novaString;
+    tam1 = strlen(s1), tam2 = strlen(s2);
+    novaString = (char *)malloc(sizeof(char)*(tam1+tam2+1));
+    if(novaString == NULL)
+    {
+        printf("Erro ao alocar memoria\n");
+        return 0;
+    }
+    strcpy(novaString, s1);
+    strcat(novaString, s2);
+
+    printf("%s", novaString);
+    free(novaString);
+
+}
 
 int main()
 {
     char *s1, *s2;
-    s1 = (char *)malloc(sizeof(char) * 7);
-    s2 = (char *)malloc(sizeof(char) * 7);
-    strcpy(s1, "arthur");
-    strcpy(s2, "melo");
+    s1 = (char *)malloc(sizeof(char));
+    s2 = (char *)malloc(sizeof(char));
 
-    *concatena(s1, s2);
-}
+    if (s1 == NULL || s2 == NULL) {
+        printf("Erro ao alocar memória!\n");
+        return 1;
+    }
 
-char *concatena(char *s1, char *s2)
-{
-    char *novaCadeia = s1;
+    strcpy(s1, "Arthur");
+    strcpy(s2, "Melo");
 
-    int tamanho1 = strlen(s1), tamanho2 = strlen(s2);
+    char *resultado = concatena(s1, s2);
+    printf("%s\n", resultado);
 
-    novaCadeia = (char *)malloc(sizeof(char) * (tamanho1 + tamanho2));
 
-    strcpy(novaCadeia, s1);
-    strcat(novaCadeia, s2);
-    
-
-    printf("%s\n", novaCadeia);
-
+    free(s1);
+    free(s2);
     return 0;
 }
